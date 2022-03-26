66d89dee94444771f2d795e363a5c65c9086bbea
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-03-26 15:56:21 +0100

    Extract function isCorrectAnswer

--- src/index.jsx
+++ src/index.jsx
@@ -3,21 +3,15 @@ import * as ReactDOM from "react-dom";
 import {useState} from "react";
 import { randomQuestion, isCorrectAnswer } from "./questions";
 
-
-
 function Quiz() {
     const [question, setQuestion] = useState(randomQuestion());
     const [answer, setAnswer] = useState();
 
-
-
     if (answer) {
         return <>
-            <h1>{isCorrectAnswer()} ? "Right" : "Wrong"}</h1>
+            <h1>{isCorrectAnswer(question, answer)} ? "Right" : "Wrong"}</h1>
         </>;
     }
-
-
 
     return <>
         <h1>{question.question}</h1>
--- src/questions.jsx
+++ src/questions.jsx
@@ -2,7 +2,7 @@ export function randomQuestion() {
     return Questions[Math.trunc(Math.random() * Questions.length)];
 }
 
-export function isCorrectAnswer() {
+export function isCorrectAnswer(question, answer) {
     return question.correct_answers[answer + "_correct"] === "true";
 }
 
