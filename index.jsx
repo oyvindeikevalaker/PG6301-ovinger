c66d264b0e9014fb6975842ff1416536e66bbc87
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-04-09 11:59:20 +0200

    Nested routes to show correct wrong answer

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom";
 import React, {useState} from "react";
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
-import {randomQuestion} from "./questions";
+import {BrowserRouter, Link, Route, Routes, useNavigate} from "react-router-dom";
+import {randomQuestion, isCorrectAnswer} from "./questions";
 
 function FrontPage() {
     return <div>
@@ -13,14 +13,33 @@ function FrontPage() {
 }
 
 function ShowQuestion() {
+    function handleAnswer(answer){
+        if(isCorrectAnswer(question, answer)){
+            navigate("/answer/correct");
+        } else {
+            navigate("/answer/wrong");
+        }
+    }
+    const navigate = useNavigate();
     const [question] = useState(randomQuestion());
     return <div>
         <h1>{question.question}</h1>
         {Object.keys(question.answers)
             .filter(a => question.answers[a])
             .map(a => <div key={a}>
-                <button>{question.answers[a]}</button>)
+                <button onClick={() => handleAnswer(a)}>{question.answers[a]}</button>)
             </div>)};
+    </div>;
+}
+
+function ShowAnswer() {
+    return <div>
+        <Routes>
+            <Route path={"correct"} element={<h1>Correct!</h1>}/>
+            <Route path={"wrong"} element={<h1>Wrong!</h1>}/>
+        </Routes>
+        <div><Link to={"/"}>Show score</Link></div>
+        <div><Link to={"/question"}>New question</Link></div>
     </div>;
 }
 
@@ -29,6 +48,7 @@ function QuizGame(){
         <Routes>
             <Route path={"/"} element={<FrontPage/>}/>
             <Route path={"/question"} element={<ShowQuestion/>}/>
+            <Route path={"/answer/*"} element={<ShowAnswer/>}/>
         </Routes>
     </BrowserRouter>;
 }
