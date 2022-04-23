79e56e11b15081beb3db0dc212fef705ba530e6d
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-04-23 18:11:51 +0200

    Revert "npm install --save pretty"
    This reverts commit d1feed4b183259979496c44b256ddf8ec74ca447.

--- __tests__/quizGame.test.jsx
+++ __tests__/quizGame.test.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {FrontPage} from "../quizGame";
 import {MemoryRouter} from "react-router-dom";
-import pretty from "pretty";
 
 describe("Quiz game", () => {
     it("Shows answer status", () => {
@@ -12,7 +11,6 @@ describe("Quiz game", () => {
             element
         );
         expect(element.querySelector("[data-testid=status]").textContent)
-            .toEqual("You have answered 3 of 10 correctly");
-        expect(pretty(element.innerHTML)).toMatchSnapshot();
+            .toEqual("You have answered 3 of 10 correctly")
     })
 })
