ed7f4b8574bd9edbbd08958c9484128841f2988b
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-04-23 18:11:20 +0200

    Revert "added badges to README.md"
    This reverts commit 6be58691bc84ecd08ba98d63abad02944fda03e5.

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {QuizGame} from "./quizGame";
-import {BrowserRouter} from "react-router-dom"
+import {BrowseRouter} from "react-router-dom"
 
-ReactDOM.render(<BrowserRouter><QuizGame/></BrowserRouter>, document.getElementById("app"));
+ReactDOM.render(<BrowseRouter><QuizGame/></BrowseRouter>, document.getElementById("app"));
