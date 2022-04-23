8a1d3307465de71d0958a2eabf703fcc82704db3
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-04-23 18:12:43 +0200

    Revert "initial question page"
    This reverts commit aad017b15377b41c1f5df2ff322e3a23487febab.

--- index.jsx
+++ index.jsx
@@ -1,17 +1,11 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 function FrontPage() {
     return <div>
         <h1>Quiz app</h1>
-        <Link to={"/question"}><button>Take a new quiz</button></Link>
-    </div>;
-}
-
-function ShowQuestion() {
-    return <div>
-        <h1>Here is a question</h1>
+        <button>Take a new quiz</button>
     </div>;
 }
 
@@ -19,7 +13,6 @@ function QuizGame(){
     return <BrowserRouter>
         <Routes>
             <Route path={"/"} element={<FrontPage/>}/>
-            <Route path={"/question"} element={<ShowQuestion/>}/>
         </Routes>
     </BrowserRouter>
 }
