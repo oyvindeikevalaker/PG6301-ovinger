ca696c0e15fdd0638df09da7749d5f2b7d2c64bf
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-04-23 18:12:24 +0200

    Revert "useState to keep track of correct and total answers"
    This reverts commit db93c1d48ce1903082d20d42453d21307c52232f.

--- index.jsx
+++ index.jsx
@@ -3,21 +3,18 @@ import React, {useState} from "react";
 import {BrowserRouter, Link, Route, Routes, useNavigate} from "react-router-dom";
 import {randomQuestion, isCorrectAnswer} from "./questions";
 
-function FrontPage({correctAnswers, questionsAnswered}) {
+function FrontPage() {
     return <div>
         <h1>Quiz app</h1>
-        <div>You have answered {correctAnswers} of {questionsAnswered} correctly</div>
         <Link to={"/question"}>
             <button>Take a new quiz</button>
         </Link>
     </div>;
 }
 
-function ShowQuestion({setCorrectAnswers, setQuestionsAnswered}) {
+function ShowQuestion() {
     function handleAnswer(answer){
-        setQuestionsAnswered(q => q +1);
         if(isCorrectAnswer(question, answer)){
-            setCorrectAnswers(q => q + 1);
             navigate("/answer/correct");
         } else {
             navigate("/answer/wrong");
@@ -30,8 +27,8 @@ function ShowQuestion({setCorrectAnswers, setQuestionsAnswered}) {
         {Object.keys(question.answers)
             .filter(a => question.answers[a])
             .map(a => <div key={a}>
-                <button onClick={() => handleAnswer(a)}>{question.answers[a]}</button>
-            </div>)}
+                <button onClick={() => handleAnswer(a)}>{question.answers[a]}</button>)
+            </div>)};
     </div>;
 }
 
@@ -43,18 +40,14 @@ function ShowAnswer() {
         </Routes>
         <div><Link to={"/"}>Show score</Link></div>
         <div><Link to={"/question"}>New question</Link></div>
-    </div>
+    </div>;
 }
 
 function QuizGame(){
-    const [questionsAnswered, setQuestionsAnswered] = useState(0);
-    const [correctAnswers, setCorrectAnswers] = useState(0);
     return <BrowserRouter>
         <Routes>
-            <Route path={"/"}
-                   element={<FrontPage questionsAnswered={questionsAnswered} correctAnswers={correctAnswers}/>}/>
-            <Route path={"/question"}
-                   element={<ShowQuestion setQuestionsAnswered={setQuestionsAnswered} setCorrectAnswers={setCorrectAnswers}/>}/>
+            <Route path={"/"} element={<FrontPage/>}/>
+            <Route path={"/question"} element={<ShowQuestion/>}/>
             <Route path={"/answer/*"} element={<ShowAnswer/>}/>
         </Routes>
     </BrowserRouter>;
