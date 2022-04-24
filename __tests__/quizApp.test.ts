4c1b124e3162eaf64c672b34b9736a696f8a478f
oyvindeikevalaker <89633763+oyvindeikevalaker@users.noreply.github.com>
2022-04-24 10:16:26 +0200

    Jest test for GET /quiz/answer correct (failed)

--- __tests__/quizApp.test.ts
+++ __tests__/quizApp.test.ts
@@ -15,4 +15,10 @@ describe("The quiz broadcast", () => {
     });
     expect(response.body).not.toHaveProperty("correct_answers");
   });
+  it("responst to correct answers", async () => {
+    await request(app)
+      .post("/quiz/answer")
+      .send({ id: 974, answer: "answer_b" })
+      .expect({ result: "correct" });
+  });
 });
